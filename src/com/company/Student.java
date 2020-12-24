37decc5264e65116d5c5aaff1e9d7b5e3ab2d627
luyouyi <1925445089@qq.com>
2020-12-24 15:08:17 +0800

    第一次

---
+++ src/com/company/Student.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * @author 夜游症
+ * @version 1.0
+ * @descript:
+ * @date 2020/12/24/14:52
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("SFG");
+    }
+}
